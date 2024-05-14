18f3b020d6eb1bf777b3aafe43445941415ed785
luigitimeisreal <antoniors2003@gmail.com>
2024-05-14 13:02:41 +0200

    Añadidas rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RecetaComponent } from './pages/receta/receta.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import { RecetaComponent } from './pages/receta/receta.component';
 import { Receta } from './interfaces/receta';
-import { ContactoComponent } from './pages/contacto/contacto.component';
-import { ListaComponent } from './pages/lista/lista.component';
-import { AnyadirComponent } from './pages/anyadir/anyadir.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
+// import { ContactoComponent } from './pages/contacto/contacto.component';
+// import { ListaComponent } from './pages/lista/lista.component';
+// import { AnyadirComponent } from './pages/anyadir/anyadir.component';
+// import { InicioComponent } from './pages/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
-    RecetaComponent,
-    ContactoComponent,
-    ListaComponent,
-    AnyadirComponent,
-    InicioComponent
-  ],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ListaComponent } from './pages/lista/lista.component';
+import { FavRecetasComponent } from './pages/fav-recetas/fav-recetas.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {"path": "", component: InicioComponent},
+    {"path": "lista", component: ListaComponent},
+    {"path": "recetas", component: FavRecetasComponent},
+    {"path": "contacto", component: ContactoComponent},
+    {"path": "favoritos", redirectTo: "recetas"},
+
+];
